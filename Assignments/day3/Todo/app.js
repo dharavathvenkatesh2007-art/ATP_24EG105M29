8c7554874078f51645c65569ab0e2efcf5e39b1e
dharavathvenkatesh2007-art <dharavathvenkatesh2007@gmail.com>
2026-02-27 22:43:26 +0530

    Update app.js

--- Assignments/day3/Todo/app.js
+++ Assignments/day3/Todo/app.js
@@ -2,5 +2,14 @@ import { addTask,getAllTasks,completeTask} from "./task.js";
 addTask("Eating","High","2024-12-12")
 addTask("Sleep","Medium","2024-12-12")
 console.log(getAllTasks())
+/*
+PS C:\Users\dhara\JavaScript\Assignments\day3\Todo> node .\app.js
+[
+  { title: 'Eating', priority: 'High', dueDate: '2024-12-12' },
+  { title: 'Sleep', priority: 'Medium', dueDate: '2024-12-12' }
+]
+Task  Completed:1
+Task Not Found
+ */
 
 
