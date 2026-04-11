ceca6d506d2c85868975192bcd288733e5269231
dharavathvenkatesh2007-art <dharavathvenkatesh2007@gmail.com>
2026-04-11 17:27:39 +0530

    Add files via upload

---
+++ ATP/Week-1/1BigOf2num.js
@@ -0,0 +1,20 @@
+
+//1.Find the big number in given two numbers
+
+let a=10
+let b=20
+if(a>b){
+    console.log(`a:${a} is bigger`)// a if a is bigger
+}
+else if(a<b){
+    console.log(`b:${b} is bigger`)// b if b is bigger
+}
+else{
+    console.log(`a is equal to b ${b}`)
+}
+/*
+OutPut
+PS C:\Users\dhara\JavaScript\Assignments> node .\BigOf2num.js
+b:20 is bigger
+
+*/
